baa0822597a02bf0f41d78667f6d3720b35d3691
blumindsmx <48847286+blumindsmx@users.noreply.github.com>
2019-05-19 08:35:02 -0500

    x

---
+++ precache-manifest.5709b042ffdf2905d02ed5170aa67c3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23274d845a108124aa283f162218d82f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ac83c1eb69575a7b49c",
+    "url": "/static/css/main.b8023a15.chunk.css"
+  },
+  {
+    "revision": "eb8b400a4f633cd8cd4e",
+    "url": "/static/js/2.683fdace.chunk.js"
+  },
+  {
+    "revision": "0ac83c1eb69575a7b49c",
+    "url": "/static/js/main.6ac26adc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
